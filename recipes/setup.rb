a42d52302b35ebb40035d04d6ebee5f2da5f40e8
maheshrajannan <mahesh.rajannan@gmail.com>
2017-11-20 00:10:39 +0000

    added ohai

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,7 +10,13 @@ package 'git' do
 end
 
 file '/etc/motd' do
-  content 'Property of ...'
+  content "Property of ...
+  IPADDRESS: #{node['ipaddress']}
+  HOSTNAME : #{node['hostname']}
+  MEMORY   : #{node['memory']['total']}
+  CPU      : #{node['cpu']['0']['mhz']}
+  "
+  mode '0644'
   action :create
   owner 'root'
   group 'root'
