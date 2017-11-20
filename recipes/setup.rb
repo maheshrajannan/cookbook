f54d003980cc32a010909f5624d499be717a7974
maheshrajannan <mahesh.rajannan@gmail.com>
2017-11-20 01:10:13 +0000

    template added

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -9,15 +9,6 @@ package 'git' do
   action :install
 end
 
-file '/etc/motd' do
-  content "Property of ...
-  IPADDRESS: #{node['ipaddress']}
-  HOSTNAME : #{node['hostname']}
-  MEMORY   : #{node['memory']['total']}
-  CPU      : #{node['cpu']['0']['mhz']}
-  "
-  mode '0644'
-  action :create
-  owner 'root'
-  group 'root'
+template '/etc/motd' do
+  source 'motd.erb'
 end
